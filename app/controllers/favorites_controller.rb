8e55651babd7b81bf9f5814e5fe67f5573a0d47e
EC2 Default User <ec2-user@ip-172-31-30-254.us-east-2.compute.internal>
2021-05-10 12:31:55 +0000

    Initial Commit

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,13 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     @favorite = current_user.favorites.new(book_id: params[:book_id])
     @favorite.save
-     redirect_back(fallback_location: root_path)
+   
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     @favorite = Favorite.find_by(book_id: params[:book_id], user_id: current_user.id)
     @favorite.destroy
-     redirect_back(fallback_location: root_path)
+   
   end
 end
--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -4,12 +4,12 @@ class PostCommentsController < ApplicationController
    @post_comment = current_user.post_comments.new(post_comments_params)
    @post_comment.book_id = @book.id
    @post_comment.save
-   redirect_to book_path(@book)
+
   end
 
   def destroy
     PostComment.find_by(id: params[:id],book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
   end
 
 private
